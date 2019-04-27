04a565d2b64b1dcb04fbefca1dd8fb728d5b8a2b
malkebu-lan <malkebu-lan@uequations.com>
2019-04-27 13:09:12 -0400

    add await

--- src/main/scala/com/uequations/UEQServer.scala
+++ src/main/scala/com/uequations/UEQServer.scala
@@ -3,9 +3,13 @@ package com.uequations
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 object UEQServer extends App {
 
   implicit lazy val system = ActorSystem()
   implicit lazy val materializer = ActorMaterializer()(system)
 
+  Await.result(system.whenTerminated, Duration.Inf)
 }
